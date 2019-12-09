2a498fb604b04a77f8481a79f6e8927549d39ede
idmyn <hello@davidmyno.rs>
2019-12-09 17:10:39 +0000

    implement content method for Story

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,2 +1,10 @@
 class Story < ApplicationRecord
+  has_many :story_words
+  has_many :readings
+  has_many :students, through: :readings
+
+  def content
+    sorted = story_words.sort { |a, b| a.index <=> b.index }.map(&:content)
+    sorted.join(" ")
+  end
 end
