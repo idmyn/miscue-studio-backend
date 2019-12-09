3be204e2f90aacaa8e94a1624165d8ef0d32fa24
idmyn <hello@davidmyno.rs>
2019-12-09 15:55:33 +0000

    add faker, enable bcrypt, and fill Student and Teacher relationships

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,7 @@
 class Student < ApplicationRecord
   belongs_to :teacher
+  has_many :readings
+  has_many :stories, through: :readings
+  # has_many :words, through: :readings
+  # has_many :mistakes, through: :readings
 end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,2 +1,4 @@
 class Teacher < ApplicationRecord
+  has_secure_password
+  has_many :students
 end
