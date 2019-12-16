e46489ad0b014e3214a5bb457db31ad7856cb04e
idmyn <hello@davidmyno.rs>
2019-12-16 17:06:23 +0000

    add ‘parent_word’ to story_words in story serializer
    this is so that frontend can list the clean ‘parent_word’ without punctuation in
    the list of mistakes

--- app/serializers/story_serializer.rb
+++ app/serializers/story_serializer.rb
@@ -2,7 +2,7 @@ class StorySerializer < ActiveModel::Serializer
   attributes :id, :title, :author, :content
   def content
     object.story_words.map do |story_word|
-      { id: story_word.id, wordId: story_word.word_id, content: story_word.content }
+      { id: story_word.id, parent_word: story_word.word.content, content: story_word.content }
     end
   end
 end
