0432875bf9c607692bb086ef8faa2af442497de7
idmyn <hello@davidmyno.rs>
2019-12-19 14:51:36 +0000

    add microsoft azure host config and remove local_env config

--- config/application.rb
+++ config/application.rb
@@ -41,15 +41,8 @@ module MiscueStudioBackend
       end
     end
 
-    config.before_configuration do
-      env_file = File.join(Rails.root, "config", "local_env.yml")
-      YAML.safe_load(File.open(env_file)).each do |key, value|
-        ENV[key.to_s] = value
-      end if File.exist?(env_file)
-    end
-
     Rails.application.configure do
-      config.hosts << "miscue-studio.pdfhwxygmg.eu-west-2.elasticbeanstalk.com"
+      config.hosts << "miscue-studio.uksouth.azurecontainer.io"
     end
   end
 end
